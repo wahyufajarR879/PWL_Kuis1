7f7c21584e75e96f542dff34dac03a54cad0f79a
=juslev <reyzae99@gmail.com>
2021-03-13 23:51:01 +0700

    membenarkan model selesai.

--- app/Models/pwl_kuis_1.php
+++ app/Models/pwl_kuis_1.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class pwl_kuis_1 extends Model
 {
     use HasFactory;
+    public $table = '_daftar__barang';
+    public $table = '_daftar__pegawai';
+    public $table = '_daftar__pelanggan';
+    public $table = '_daftar__supplier';
 }
